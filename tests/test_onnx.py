e5c13a39b02f19f974b02b8720039dae67ea610b
mwessley <matthias.wess@gmail.com>
2021-04-13 19:45:06 +0200

    added first working onnx importer

---
+++ tests/test_onnx.py
@@ -0,0 +1,35 @@
+import pytest
+import sys
+sys.path.append("./")
+from pathlib import Path
+import logging
+import os
+
+from annette import get_database
+from annette.onnx_graph import ONNXGraph
+from annette.graph import AnnetteGraph
+
+__author__ = "Matthias Wess"
+__copyright__ = "Christian Doppler Laboratory for Embedded Machine Learning"
+__license__ = "Apache 2.0"
+
+def test_ONNXGraph_to_annette(network="cf_resnet50",inputs=None):
+    network_file = get_database('graphs','onnx',network+'.onnx')
+    print("ONNXGraph_test")
+    onnx_network = ONNXGraph(network_file)
+    annette_graph = onnx_network.onnx_to_annette(network, inputs)
+    json_file = get_database( 'graphs', 'annette',
+                     annette_graph.model_spec["name"]+'.json')
+    annette_graph.to_json(json_file)
+
+    assert True
+    return 0 
+
+def main():
+    print("main")
+    network = "Resnet_original"
+    test_ONNXGraph_to_annette('resnet18-v1-7',['data'])
+
+
+if __name__ == '__main__':
+    main()
